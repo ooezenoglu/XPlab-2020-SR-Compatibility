bc12ce686a7565a7a931ef5011486f448060090b
ElBuberino <61584873+ElBuberino@users.noreply.github.com>
2020-07-07 16:45:49 +0200

    bugfix

--- experiment/01_pilot/03_custom_views_templates.js
+++ experiment/01_pilot/03_custom_views_templates.js
@@ -21,150 +21,6 @@ const custom_views = {};
 // Custom view template for trials
 custom_views.keypress_rotation = function(config) {
     const keypress_rotation_function = {
-        name: config.name,
-        title: magpieUtils.view.setter.title(config.title, ""),
-        render: function(CT, magpie) {
-            let startingTime;
-            const question = magpieUtils.view.setter.question(
-                config.data[CT].question
-            );
-            const key1 = config.key1;
-            const key2 = config.key2;
-            const value1 = config[key1];
-            const value2 = config[key2];
-            const viewTemplate = `<div class="magpie-view">
-                    <h1 class='magpie-view-title'>${this.title}</h1>
-                    <p class='magpie-response-keypress-header'><strong>${key1}</strong> = ${value1}, <strong>${key2}</strong> = ${value2}</p>
-                    <p class='magpie-response-keypress-header' id='feedback'></p>
-                    <div class='magpie-view-stimulus-container'>
-                        <div class='magpie-view-stimulus magpie-nodisplay'></div>
-                    </div>
-                </div>`;
-            const answerContainerElem = `<div class='magpie-view-answer-container'>
-                        <p class='magpie-view-question'>${question}</p>
-                          </div>`;
-
-            $("#main").html(viewTemplate);
-
-            const handleKeyPress = function(e) {
-                const keyPressed = String.fromCharCode(
-                    e.which
-                ).toLowerCase();
-
-                if (keyPressed === key1 || keyPressed === key2) {
-                    let correctness;
-                    const RT = Date.now() - startingTime; // measure RT before anything else
-					
-					// clear old timeouts and remove them from the timeout array
-					  clearTimeout(window.timeout[0]);
-					  window.timeout.shift();
-					  clearTimeout(window.timeout[0]);
-					  window.timeout.shift();
-				
-
-                    if (
-                        config.data[CT].expected ===
-                            config[keyPressed]
-                    ) {
-                        correctness = "correct";
-                        // show blank screen
-                        $(".magpie-view-stimulus").addClass("magpie-invisible");
-                        $('#feedback').text('');
-
-                    } 
-					
-					else if (RT > 3500) {
-					
-					}
-										
-					else {
-                        correctness = "incorrect";
-                        // show feedback
-                        $(".magpie-view-stimulus").addClass("magpie-invisible");
-                        $('#feedback').text('Incorrect answer!');
-                    }
-					
-
-                    const trial_data = {
-                        trial_type: config.trial_type,
-                        trial_number: CT + 1,
-                        key_pressed: keyPressed,
-                        correctness: correctness,
-                        RT: RT
-                    };
-
-
-                    for (let prop in config.data[CT]) {
-                        if (config.data[CT].hasOwnProperty(prop)) {
-                            trial_data[prop] = config.data[CT][prop];
-                        }
-                    }
-
-                    trial_data[config.data[CT].key1] =
-                        config.data[CT][key1];
-                    trial_data[config.data[CT].key2] =
-                        config.data[CT][key2];
-
-                    if (config.data[CT].picture !== undefined) {
-                        trial_data.picture = config.data[CT].picture;
-                    }
-
-                    if (config.data[CT].canvas !== undefined) {
-                        if (config.data[CT].canvas.canvasSettings !== undefined) {
-                            for (let prop in config.data[CT].canvas.canvasSettings) {
-                                if (config.data[CT].canvas.canvasSettings.hasOwnProperty(prop)) {
-                                    trial_data[prop] = config.data[CT].canvas.canvasSettings[prop];
-                                }
-                            }
-                            delete trial_data.canvas.canvasSettings;
-                        }
-                        for (let prop in config.data[CT].canvas) {
-                            if (config.data[CT].canvas.hasOwnProperty(prop)) {
-                                trial_data[prop] = config.data[CT].canvas[prop];
-                            }
-                        }
-                        delete trial_data.canvas;
-                    }
-
-                    magpie.trial_data.push(trial_data);
-                    $("body").off("keydown", handleKeyPress);
-                    setTimeout(magpie.findNextView, 400); // delay to accomodate feedback
-                }
-            };
-
-            const enableResponse = function() {
-                $(".magpie-view").append(answerContainerElem);
-                $("body").on("keydown", handleKeyPress);
-            };
-
-            startingTime = Date.now();
-
-            // creates the DOM of the trial view
-            magpieUtils.view.createTrialDOM(
-                {
-                    pause: config.pause,
-                    fix_duration: config.fix_duration,
-                    stim_duration: config.stim_duration,
-                    data: config.data[CT],
-                    evts: config.hook,
-                    view: "keyPress"
-                },
-                enableResponse
-            );
-        },
-        CT: 0,
-        trials: config.trials
-    };
-
-<<<<<<< Updated upstream
-    return keypress_rotation_practice_function;
-};
-
-
-
-// Custom view template for main/experimental trials
-custom_views.keypress_rotation_main = function(config) {
-    const keypress_rotation_main_function = {
        name: config.name,
         title: magpieUtils.view.setter.title(config.title, ""),
         render: function(CT, magpie) {
@@ -194,6 +50,8 @@ custom_views.keypress_rotation_main = function(config) {
                 const keyPressed = String.fromCharCode(
                     e.which
                 ).toLowerCase();
+				
+
 
                 if (keyPressed === key1 || keyPressed === key2) {
                     let correctness;
@@ -298,9 +156,5 @@ custom_views.keypress_rotation_main = function(config) {
         trials: config.trials
     };
 
-    return keypress_rotation_main_function;
-};
-=======
     return keypress_rotation_function;
 };
->>>>>>> Stashed changes

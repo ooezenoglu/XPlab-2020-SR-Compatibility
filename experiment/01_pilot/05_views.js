8e452150a5d71ae66eb76fe2dc626878cf937e8b
Manicxxx <mewert@uni-osnabrueck.de>
2020-07-10 14:20:10 +0200

    added instruction text

--- experiment/01_pilot/05_views.js
+++ experiment/01_pilot/05_views.js
@@ -51,6 +51,7 @@ const instructions_general = magpieViews.view_generator("instructions", {
          We will <strong>practice</strong> this first.
          <br />
          <br />
+         Please make sure that you will <strong>not get disturbed</strong> in the next 10-15 minutes and that you <strong>fully concentrate</strong> on this experiment!
          <p style="text-align:center;"><img src="../materials/instructions-image.jpg" alt="instructions" height="300" width="400"></p>`,
   buttonText: 'Continue'
 });

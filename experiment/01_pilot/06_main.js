5d212ee3d423659c71f1c32125d18e10ce70b221
ooezenoglu <o.oezenoglu@outlook.de>
2020-07-11 22:48:58 +0200

    add second deploy method and ID

--- experiment/01_pilot/06_main.js
+++ experiment/01_pilot/06_main.js
@@ -123,13 +123,13 @@ else {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "INSERT_A_NUMBER",
+            experimentID: "155",
             serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "debug",
-            contact_email: "YOUREMAIL@wherelifeisgreat.you",
+            deployMethod: "directLink",
+            contact_email: "office@niklas-bub.de",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
         // Here, you can specify how the progress bar should look like
